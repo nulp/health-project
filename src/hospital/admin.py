4a33ea86dd287d7f91eae30612873ba7eb7dd474
Medezee <gorkabrusentsov@gmail.com>
2019-05-16 01:36:21 +0300

    Add tables to test db

--- src/hospital/admin.py
+++ src/hospital/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Patient, Medicament, Country, Manufacturer, Applicant
 # Register your models here.
+
+admin.site.register(Patient)
+admin.site.register(Medicament)
+admin.site.register(Country)
+admin.site.register(Manufacturer)
+admin.site.register(Applicant)
